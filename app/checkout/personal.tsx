8ef04bf7bdbe45f57f324697fe5f7d5f418f5d1f
Faysal Bsata <faysalbsata@hotmail.com>
2023-06-28 21:11:22 +0400

    controlling via schema

--- app/checkout/personal.tsx
+++ app/checkout/personal.tsx
@@ -1,9 +1,29 @@
 import { useRouter } from 'expo-router';
-import { ScrollView } from 'react-native';
-import { Button, Card, TextInput, useTheme } from 'react-native-paper';
+import { ScrollView, View } from 'react-native';
+import {
+  Button,
+  Card,
+  HelperText,
+  TextInput,
+  useTheme,
+} from 'react-native-paper';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  PersonalInfo,
+  PersonalInfoSchema,
+} from '../../src/schema/checkout.schema';
 export default function PersonalDetails() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PersonalInfo>({
+    resolver: zodResolver(PersonalInfoSchema),
+  });
   const router = useRouter();
-  const nextPage = () => {
+  const nextPage = (data) => {
+    console.log(data);
     router.push('/checkout/delivery');
   };
   const theme = useTheme();
@@ -20,19 +40,52 @@ export default function PersonalDetails() {
       <Card style={{ backgroundColor: theme.colors.background }}>
         <Card.Title title="Personal Information" titleVariant="titleLarge" />
         <Card.Content style={{ gap: 10 }}>
-          <TextInput
-            placeholder="John Doe"
-            label={'Name'}
-            style={{ backgroundColor: theme.colors.background }}
+          <Controller
+            {...{ control }}
+            name="name"
+            render={({
+              field: { value, onChange, onBlur },
+              fieldState: { error, invalid },
+            }) => (
+              <View>
+                <TextInput
+                  placeholder="John Doe"
+                  label={'Name'}
+                  style={{ backgroundColor: theme.colors.background }}
+                  {...{ value, onBlur }}
+                  onChangeText={onChange}
+                  error={invalid}
+                />
+                <HelperText type="error" visible={invalid}>
+                  {error?.message}
+                </HelperText>
+              </View>
+            )}
           />
-          <TextInput
-            placeholder="hello@world.com"
-            label={'E-Mail'}
-            style={{ backgroundColor: theme.colors.background }}
+          <Controller
+            {...{ control }}
+            name="name"
+            render={({
+              field: { value, onChange, onBlur },
+              fieldState: { error, invalid },
+            }) => (
+              <View>
+                <TextInput
+                  placeholder="hello@world.com"
+                  label={'E-Mail'}
+                  style={{ backgroundColor: theme.colors.background }}
+                  {...{ value, onBlur }}
+                  onChangeText={onChange}
+                />
+                <HelperText type="error" visible={invalid}>
+                  {error?.message}
+                </HelperText>
+              </View>
+            )}
           />
         </Card.Content>
       </Card>
-      <Button mode="contained" onPress={nextPage}>
+      <Button mode="contained" onPress={handleSubmit(nextPage)}>
         Next
       </Button>
     </ScrollView>
